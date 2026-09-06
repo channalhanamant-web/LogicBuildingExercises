420c31ade0dfa48bc6b00dabba65b03a2477f6bb
Hanamant <channal.hanamant@gmail.com>
2026-09-06 22:19:22 +0530

    Key change

---
+++ src/com/LogicBuildingSecond/ChangingKeyCountInAString.java
@@ -0,0 +1,18 @@
+package com.LogicBuildingSecond;
+
+public class ChangingKeyCountInAString {
+
+	public static void main(String[] args) {
+		String inputString="abBcaad";
+		char inputArray[]=inputString.toLowerCase().toCharArray();
+		int count=0;
+		for (int i = 1; i < inputArray.length; i++) {
+			if (inputArray[i-1]!=inputArray[i]) {
+				count++;
+			}
+		}
+		System.out.println(count+" times char changed");
+
+	}
+
+}
